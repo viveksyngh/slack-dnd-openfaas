fc35438e8589b873b73cbaecf53ff67e5e4910b8
Vivek Singh <vivekkmr45@yahoo.in>
2018-06-17 03:39:45 +0530

    Updated end-snooze and set-snooze functions
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- end-snooze/handler.go
+++ end-snooze/handler.go
@@ -1,10 +1,59 @@
 package function
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
+
+//Response struct response for
+type Response struct {
+	OK               bool `json:"ok"`
+	DnDEnabled       bool `json:"dnd_enabled"`
+	NextDnDStartTime int  `json:"next_dnd_start_ts"`
+	NextDnDEndTime   int  `json:"next_dnd_end_ts"`
+	SnoozeEnabled    bool `json:"snooze_enabled"`
+}
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+	URL := "https://slack.com/api/dnd.endSnooze"
+	slackToken := os.Getenv("slack_token")
+	if slackToken == "" {
+		os.Stderr.WriteString("Slack token is not set")
+		return ""
+	}
+	client := http.Client{}
+
+	request, _ := http.NewRequest(http.MethodPost, URL, nil)
+	request.Header.Add("Authorization", "Bearer "+slackToken)
+	request.Header.Add("Content-type", "application/x-www-form-urlencoded")
+
+	res, err := client.Do(request)
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		return "Something went wrong: " + err.Error()
+	}
+
+	if res.StatusCode == 200 {
+		defer res.Body.Close()
+
+		responseBytes, _ := ioutil.ReadAll(res.Body)
+		os.Stderr.WriteString("Response: " + string(responseBytes))
+		var resp Response
+		err = json.Unmarshal(responseBytes, &resp)
+
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			return "Something went wrong"
+		}
+
+		if resp.OK == true {
+			return "Snooze has been disabled."
+		}
+		return "Failed to end snooze, Something went wrong."
+	}
+
+	return "Failed to end snooze, Something went wrong."
 }
--- set-snooze/handler.go
+++ set-snooze/handler.go
@@ -1,10 +1,63 @@
 package function
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
+
+//Request struct for request body
+type Request struct {
+	NumberOfMinutes int `json:"num_minutes"`
+}
+
+//Response struct for response body
+type Response struct {
+	OK              bool `json:"ok"`
+	SnoozeEnabled   bool `json:"snooze_enabled"`
+	SnoozeEndtime   int  `json:"snooze_endtime"`
+	SnoozeRemaining int  `json:"snooze_remaining"`
+}
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+	URL := "https://slack.com/api/dnd.setSnooze"
+	URL = URL + "?num_minutes=" + string(req)
+
+	slackToken := os.Getenv("slack_token")
+	if slackToken == "" {
+		os.Stderr.WriteString("Slack token is not set")
+		return ""
+	}
+
+	client := http.Client{}
+	request, _ := http.NewRequest(http.MethodGet, URL, nil)
+	request.Header.Add("Authorization", "Bearer "+slackToken)
+	request.Header.Add("Content-type", "application/x-www-form-urlencoded")
+
+	res, err := client.Do(request)
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		return "Something went wrong: " + err.Error()
+	}
+
+	if res.StatusCode == 200 {
+		defer res.Body.Close()
+
+		responseBytes, _ := ioutil.ReadAll(res.Body)
+		os.Stderr.WriteString("Response: " + string(responseBytes))
+		var resp Response
+		err = json.Unmarshal(responseBytes, &resp)
+
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			return "Something went wrong"
+		}
+
+		if resp.SnoozeEnabled == true {
+			return "Snooze has been enabled for " + string(req) + " minutes"
+		}
+	}
+	return "Something went wrong"
 }
